caa4281d71f2672a8159e2ac0ae9f4d548f93db1
Darren Wong <darrenwong46@yahoo.com>
2018-04-09 19:31:27 +0800

    Stuck as retrieving picker value

--- SEBeTender/SEBeTender/HttpRequestHandler.cs
+++ SEBeTender/SEBeTender/HttpRequestHandler.cs
@@ -29,5 +29,39 @@ namespace SEBeTender
             }
             return result;
         }
+
+        public static async Task<string> SearchPostRequest(string url, string tenderReference, string tenderTitle, string originatingStation, string closingDateFrom, string closingDateTo, string biddingClosingDateFrom, string biddingClosingDateTo)
+        {
+            HttpClient client = new HttpClient();
+            string result = "";
+            var parameters = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string,string>("SchReferno", tenderReference),
+                new KeyValuePair<string,string>("SchTendertitle", tenderReference),
+                new KeyValuePair<string,string>("SchStation", originatingStation),
+                new KeyValuePair<string,string>("SchFromClosedate", closingDateFrom),
+                new KeyValuePair<string,string>("SchToClosedate", closingDateTo),
+                new KeyValuePair<string,string>("SchReferno", tenderReference),
+
+
+            });
+
+            var uri = new Uri(string.Format(url, string.Empty));
+            try
+            {                                                                                                                                                                                                                         
+                var response = await client.PostAsync(uri, parameters);
+
+                if(response.StatusCode == System.Net.HttpStatusCode.Found)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return result;
+
+        }
     }
 }
