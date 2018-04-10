bf6ebe5622c87e0b27b1d70e93ed6162d8959495
Joseph Sim <joseph_sim19@hotmail.com>
2018-04-11 01:58:34 +0800

    Revert "Revert "Revert "Revert ".""""
    This reverts commit 44ad2d34fa7693dbb3d846158fa58e35e346e6f0.

---
+++ SEBeTender/SEBeTender/viewCart.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SEBeTender
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class viewCart : ContentPage
+	{
+
+        public viewCart ()
+        {   
+            BindingContext = this;
+			InitializeComponent ();
+            var items = Enumerable.Range(0, 3);
+            listView.ItemsSource = items;
+
+        }
+
+        async void onConfirmButtonClicked(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Confirmation", "Are you sure you want to checkout the cart items?", "Confirm", "Cancel");
+            
+            if (answer == true)
+            {
+                await Navigation.PushAsync(new checkoutPage());
+            }
+        }
+
+        async void onClearCartClicked(object sender, EventArgs e)
+        {
+            listView.ItemsSource = null;
+            
+        }
+
+    }
+}
