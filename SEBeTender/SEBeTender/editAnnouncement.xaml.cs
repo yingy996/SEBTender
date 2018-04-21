16e2b350467424327863478cbe8d00830e607859
Joseph Sim <joseph_sim19@hotmail.com>
2018-04-21 18:28:43 +0800

    Add edit announcement post page layout

---
+++ SEBeTender/SEBeTender/editAnnouncement.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Newtonsoft.Json;
+
+namespace SEBeTender
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class editAnnouncement : ContentPage
+	{
+		public editAnnouncement ()
+		{
+            BindingContext = this;
+            InitializeComponent();
+            
+            editTitle.Text = "aaaa";
+            editContent.Text = "aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa aaaa";
+
+            Console.WriteLine(getAnnouncementsResult());
+
+        }
+
+
+
+    }
+}
