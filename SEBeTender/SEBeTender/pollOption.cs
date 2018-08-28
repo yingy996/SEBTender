593cdcb4e7c9c407ac8622c103183816c66aab1d
yingy996 <4334078@students.swinburne.edu.my>
2018-08-28 23:27:27 +0800

    *Added "view poll" feature in mobile application

---
+++ SEBeTender/SEBeTender/pollOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEBeTender
+{
+    public class pollOption
+    {
+        public string optionID { get; set; }
+        public string optionTitle { get; set; }
+        public string pollID { get; set; }
+        public string votesCount { get; set; }
+    }
+}
