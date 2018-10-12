5883815727c8b5bee82c7e91b33211c7dad525cf
yingy996 <4334078@students.swinburne.edu.my>
2018-10-12 22:58:24 +0800

    *Completed displaying tenders retrieved from server

---
+++ SEBeTender/SEBeTender/scrapped_tender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEBeTender
+{
+    public class scrapped_tender
+    {
+        public string id { get; set; }
+        public string reference { get; set; }
+        public string title { get; set; }
+        public string category { get; set; }
+        public string originatingSource { get; set; }
+        public string tenderSource{ get; set; }
+        public string agency { get; set; }
+        public string closingDate { get; set; }
+        public string startDate { get; set; }
+        public string docInfoJson { get; set; }
+        public string originatorJson { get; set; }
+        public string fileLinks { get; set; }
+    }
+}
--- SEBeTender/SEBeTender/tenderDetailPage.xaml.cs
+++ SEBeTender/SEBeTender/tenderDetailPage.xaml.cs
@@ -13,51 +13,153 @@ namespace SEBeTender
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class tenderDetailPage : ContentPage
 	{
-		public tenderDetailPage ()
-		{
-			
+        bool isOriginatorInfoPresent = false;
+        public tenderDetailPage ()
+		{			
 		}
 
         public tenderDetailPage(tenderItem aTenderItem)
         {
+            BindingContext = this;
+            
             InitializeComponent();
-
-            string[] words = Regex.Split(aTenderItem.ClosingDate, ": ");
-            string closingDate = words[1];
+            int currentGridRow;     
+            /*string closingDate = "";
+            if (aTenderItem.Company == "Sarawak Energy")
+            {
+                isOriginatorInfoPresent = true;
+                string[] words = Regex.Split(aTenderItem.ClosingDate, ": ");
+                closingDate = words[1];
+            }*/
             
             tenderRefLbl.Text = aTenderItem.Reference;
             tenderTitleLbl.Text = aTenderItem.Title;
             oriStationLbl.Text = aTenderItem.OriginatingStation;
-            closingDateLbl.Text = closingDate;
+            closingDateLbl.Text = aTenderItem.ClosingDate;
             bidCloseDateLbl.Text = aTenderItem.BidClosingDate;
             feeBeforeGSTLbl.Text = aTenderItem.FeeBeforeGST;
             feeGSTLbl.Text = aTenderItem.FeeGST;
             feeAfterGSTLbl.Text = aTenderItem.FeeAfterGST;
-            nameLbl.Text = aTenderItem.Name;
-            officePhoneLbl.Text = aTenderItem.OffinePhone;
-            extensionLbl.Text = aTenderItem.Extension;
-            mobilePhoneLbl.Text = aTenderItem.MobilePhone;
-            emailLbl.Text = aTenderItem.Email;
-            faxLbl.Text = aTenderItem.Fax;
 
-            //Create hyperlink labels for downloadable files
-            int currentGridRow = 32;
-            foreach(KeyValuePair<string, string> file in aTenderItem.FileLinks)
+            if (aTenderItem.Company == "Sarawak Energy")
             {
-                Console.WriteLine("Key: "+ file.Key);
-                //create the hyperlink label
-                var label = new Label { Text = file.Key, TextColor = Color.DodgerBlue, Margin = new Thickness(0,0,0,5)};
-                var tapGestureRecognizer = new TapGestureRecognizer();
-                tapGestureRecognizer.Tapped += (s, e) => { Device.OpenUri(new Uri(file.Value)); };
-                label.GestureRecognizers.Add(tapGestureRecognizer);
+                nameLbl.Text = aTenderItem.Name;
+                officePhoneLbl.Text = aTenderItem.OffinePhone;
+                extensionLbl.Text = aTenderItem.Extension;
+                mobilePhoneLbl.Text = aTenderItem.MobilePhone;
+                emailLbl.Text = aTenderItem.Email;
+                faxLbl.Text = aTenderItem.Fax;
+            } else
+            {
+                displayGrid.Children.Remove(bidCloseDateTitleLbl);
+                displayGrid.Children.Remove(bidCloseLayout);
+                displayGrid.Children.Remove(boxView11);
+                displayGrid.Children.Remove(docFeeTitleLbl);
+                displayGrid.Children.Remove(feeBeforeGSTLbl);
+                displayGrid.Children.Remove(boxView13);
+                displayGrid.Children.Remove(gstTitleLbl);
+                displayGrid.Children.Remove(feeGSTLbl);
+                displayGrid.Children.Remove(boxView15);
+                displayGrid.Children.Remove(feeAfterGSTTitleLbl);
+                displayGrid.Children.Remove(feeAfterGSTLbl);
+                displayGrid.Children.Remove(boxView17);
 
-                //add hyperlink label into grid
-                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });               
+                displayGrid.Children.Remove(originatorTitleLbl);
+                displayGrid.Children.Remove(nameTitleLbl);
+                displayGrid.Children.Remove(offPhoneTitleLbl);
+                displayGrid.Children.Remove(extTitleLbl);
+                displayGrid.Children.Remove(mobPhoneTitleLbl);
+                displayGrid.Children.Remove(emailTitleLbl);
+                displayGrid.Children.Remove(faxTitleLbl);
 
-                displayGrid.Children.Add(label, 0, currentGridRow);
-                Grid.SetColumnSpan(label, 2);
-                currentGridRow++;
+                displayGrid.Children.Remove(nameLbl);
+                displayGrid.Children.Remove(officePhoneLbl);
+                displayGrid.Children.Remove(extensionLbl);
+                displayGrid.Children.Remove(mobilePhoneLbl);
+                //displayGrid.Children.Remove(emailLbl);
+                displayGrid.Children.Remove(emailLayout);
+                displayGrid.Children.Remove(faxLbl);
+
+                displayGrid.Children.Remove(boxView20);
+                displayGrid.Children.Remove(boxView22);
+                displayGrid.Children.Remove(boxView24);
+                displayGrid.Children.Remove(boxView26);
+                displayGrid.Children.Remove(boxView28);
+                displayGrid.Children.Remove(boxView30);
+
+                for (int i = 0; i < 21; i++)
+                {
+                    displayGrid.RowDefinitions.RemoveAt(displayGrid.RowDefinitions.Count()-1);
+                }
             }
+
+            Console.WriteLine("Tender source is : " + aTenderItem.TenderSource);
+            if (aTenderItem.TenderSource == "1") 
+            {
+                //If tender is from myProcurement, display its category and agency
+                currentGridRow = displayGrid.RowDefinitions.Count();
+
+                //Create labels for category and add into the grid
+                var categoryLabel = new Label { Text = "Category: ", Margin = new Thickness(0, 5, 0, 0), FontAttributes = FontAttributes.Bold };
+                var categoryDataLabel = new Label { Text = aTenderItem.Category, Margin = new Thickness(0, 5, 0, 0) };
+                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                displayGrid.Children.Add(categoryLabel, 0, currentGridRow);
+                displayGrid.Children.Add(categoryDataLabel, 1, currentGridRow);
+                currentGridRow++;
+
+                //Create boxView as separator
+                var categoryBoxView = new BoxView { BackgroundColor = Color.LightGray, HeightRequest = 1, Margin = new Thickness(0, 5, 0, 0), VerticalOptions = LayoutOptions.End, HorizontalOptions = LayoutOptions.FillAndExpand };
+                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                displayGrid.Children.Add(categoryBoxView, 0, currentGridRow);
+                Grid.SetColumnSpan(categoryBoxView, 2);
+                currentGridRow++;
+
+                //Create labels for agency and add into the grid
+                var agencyLabel = new Label { Text = "Agency: ", Margin = new Thickness(0, 5, 0, 0), FontAttributes = FontAttributes.Bold };
+                var agencyLayout = new StackLayout { Margin = new Thickness(0, 5, 0, 0) };               
+                var agencyDataLabel = new Label { Text = aTenderItem.Agency };
+                agencyLayout.Children.Add(agencyDataLabel);
+
+                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                displayGrid.Children.Add(agencyLabel, 0, currentGridRow);
+                displayGrid.Children.Add(agencyLayout, 1, currentGridRow);
+                currentGridRow++;
+
+                //Create boxView as separator
+                var agencyBoxView = new BoxView { BackgroundColor = Color.LightGray, HeightRequest = 1, Margin = new Thickness(0, 5, 0, 0), VerticalOptions = LayoutOptions.End, HorizontalOptions = LayoutOptions.FillAndExpand };
+                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                displayGrid.Children.Add(agencyBoxView, 0, currentGridRow);
+                Grid.SetColumnSpan(agencyBoxView, 2);
+            }
+            
+            //Create hyperlink labels for downloadable files
+            if (aTenderItem.FileLinks.Count > 0)
+            {
+                currentGridRow = displayGrid.RowDefinitions.Count();
+                Console.WriteLine("Current grid row : " + currentGridRow);
+                //<Label Text="Downloadable Files: " x:Name="downloadLbl" Grid.Row="31" Grid.Column="0" FontSize="16" Grid.ColumnSpan="2" FontAttributes="Bold" Margin="0,20,0,10"/>
+                var downloadLabel = new Label { Text = "Downloadable Files: ", Margin = new Thickness(0, 20, 0, 10), FontSize = 16, FontAttributes = FontAttributes.Bold };
+                displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                displayGrid.Children.Add(downloadLabel, 0, currentGridRow);
+                Grid.SetColumnSpan(downloadLabel, 2);
+                currentGridRow++;
+                foreach (KeyValuePair<string, string> file in aTenderItem.FileLinks)
+                {
+                    Console.WriteLine("Key: " + file.Key + "row: " + currentGridRow);
+                    //create the hyperlink label
+                    var label = new Label { Text = file.Key, TextColor = Color.DodgerBlue, Margin = new Thickness(0, 0, 0, 5) };
+                    var tapGestureRecognizer = new TapGestureRecognizer();
+                    tapGestureRecognizer.Tapped += (s, e) => { Device.OpenUri(new Uri(file.Value)); };
+                    label.GestureRecognizers.Add(tapGestureRecognizer);
+
+                    //add hyperlink label into grid
+                    displayGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+
+                    displayGrid.Children.Add(label, 0, currentGridRow);
+                    Grid.SetColumnSpan(label, 2);
+                    currentGridRow++;
+                }
+            }            
         }
 
         public tenderDetailPage(tenderBookmark tenderBookmark)
